130d92703024df34ac9187cd7d0cbf5ad1781183
Taurus Canis <acdole09@gmail.com>
2020-09-02 21:51:01 -0400

    removed context processor from settings.py

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -47,7 +47,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                # 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
@@ -64,6 +64,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
 
 DATABASES = {
     # "default": {
