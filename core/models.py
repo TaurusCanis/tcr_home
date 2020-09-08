03cf93952ca98a3de682f1d381d462ae11215a62
Taurus Canis <acdole09@gmail.com>
2020-09-08 11:31:33 -0400

    increased length of email field in Customer model from 20 to 200

--- core/models.py
+++ core/models.py
@@ -171,7 +171,7 @@ class Order(models.Model):
 class Customer(models.Model):
     first_name = models.CharField(max_length=20, blank=True, null=True)
     last_name = models.CharField(max_length=20, blank=True, null=True)
-    email_address = models.CharField(max_length=20, blank=True, null=True)
+    email_address = models.CharField(max_length=200, blank=True, null=True)
     # phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     # def __str__(self):
