93bdc65f93cddd7e84bb1bece82e701736b48b74
Taurus Canis <acdole09@gmail.com>
2020-09-16 13:22:48 -0400

    reverted settings back to original single file and updated wsgi.py and manage.py; changed settings dirsfiles

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -65,7 +65,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
