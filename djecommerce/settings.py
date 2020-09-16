5b0e3249e69ed45455208c8df1903e77b456259b
Taurus Canis <acdole09@gmail.com>
2020-09-16 14:12:06 -0400

    added get.secret_key to settings

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -8,7 +8,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'production')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 # SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj')
-# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 ALLOWED_HOSTS = ['tauruscanisrex.com', 'tauruscanisrex.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
