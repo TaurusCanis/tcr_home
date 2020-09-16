1114557526472947adb86bbe0afeaaeae08356b0
Taurus Canis <acdole09@gmail.com>
2020-09-16 18:31:37 -0400

    added staticfilestorage (not whitenoise) to settings

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -67,6 +67,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
