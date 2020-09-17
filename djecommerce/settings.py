67623bc8c28d067072d4176b5ca3e777c2df1738
Taurus Canis <acdole09@gmail.com>
2020-09-16 20:41:10 -0400

    changed static

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -193,10 +193,19 @@ EMAIL_HOST_PASSWORD = "SenorCerdo2013!"
 EMAIL_USE_SSL = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
