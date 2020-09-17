7eba5399ef52498ebca1f85afabb85c41e5006bc
Taurus Canis <acdole09@gmail.com>
2020-09-17 15:05:29 -0400

    set request.session['id'] to None after successful payment

--- core/models.py
+++ core/models.py
@@ -207,7 +207,7 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    braintree_transaction_id = models.ForeignKey(max_length=100)
+    braintree_transaction_id = models.CharField(max_length=100)
 
     # def __str__(self):
     #     return self.session_id
