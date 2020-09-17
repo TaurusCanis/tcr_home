5b1594283fd2b64b1b5ca4afdf706a5aad0b0957
Taurus Canis <acdole09@gmail.com>
2020-09-17 13:31:08 -0400

    DEBUG=TRUE

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -1,9 +1,9 @@
 import os
 import django_heroku
 
-ENVIRONMENT = os.getenv('ENVIRONMENT', 'production')
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-# DEBUG = True
+DEBUG = True
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
