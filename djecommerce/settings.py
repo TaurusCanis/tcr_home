9052704f435023183a01e82b859565dc6d07c5a6
Taurus Canis <acdole09@gmail.com>
2020-09-19 10:31:55 -0400

    fogot comma

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE', ),
-        'NAME': os.environ.get('DB_NAME', )
+        'NAME': os.environ.get('DB_NAME', ),
         'USER': os.environ.get('DB_USER', ),
         'PASSWORD': os.environ.get('DB_PASSWORD', ),
         'HOST': os.environ.get('DB_HOST', ),
